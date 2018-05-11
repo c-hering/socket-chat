1a8b2b03887b254d9580debdd8d2cb09a458b090
An0rak <phos4es@gmail.com>
2018-05-11 17:58:44 -0400

    started to make socket server

---
+++ server.js
@@ -0,0 +1,14 @@
+const io = require('socket.io')();
+const port = 8000;
+
+io.on('connection', client => {
+  //new client connects, broadcast to all other clients
+  io.broadcast.emit('newMessage', {body: 'A new user has joined'});
+  //new message sent to server
+  io.on('newMessage', data => {
+    console.log(data)
+  });
+});
+
+io.listen(port);
+console.log('listening on port 8000')
